83a2c6be51f2b566262293a74c249f14559d989d
William Trevisan <williamtrev@hotmail.com>
2022-06-22 14:25:34 -0300

    feat: create BookService with changeLibraryId method

--- src/Domain/Service/BookService.php
+++ src/Domain/Service/BookService.php
@@ -2,7 +2,16 @@
 
 namespace Domain\Service;
 
+use Ramsey\Uuid\UuidInterface;
+
 class BookService
 {
+    public static function changeLibraryId(
+        array $listBooks,
+        UuidInterface $libraryId
+    ): array {
+        foreach ($listBooks as $book) $book->update(libraryId: $libraryId);
 
+        return $listBooks;
+    }
 }

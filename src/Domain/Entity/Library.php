d3af4f0cd80859ba51a70ab315e2ba991926cf0a
William Trevisan <williamtrev@hotmail.com>
2022-06-22 11:28:23 -0300

    test(library): create library entity unit tests
    Create unit tests for library entity validations, create and update

--- src/Domain/Entity/Library.php
+++ src/Domain/Entity/Library.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+namespace Domain\Entity;
+
 class Library
 {
     public function __construct(
-        protected string $id,
         protected string $name,
         protected string $email,
+        protected ?string $id = null,
     ) {}
 }
--- tests/Unit/domain/entity/LibraryUnitTest.php
+++ tests/Unit/domain/entity/LibraryUnitTest.php
@@ -1,18 +1,91 @@
 <?php
 
-namespace Domain\Entity;
+namespace Tests\Unit\Domain\Entity;
 
+use Domain\Entity\Library;
+use http\Exception\InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class LibraryUnitTest extends TestCase
 {
     /** @test */
-    public function should_one_plus_one_be_two()
+    public function should_be_throw_an_exception_if_name_received_dont_has_at_least_3_characters()
     {
-        $payload = [1, 1];
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The name must be at least 3 characters');
 
-        $sum = array_reduce($payload, fn(int $carry, int $item) => $carry += $item, 0);
+        $payload = ['name' => 'Li', 'email' => 'email@library.com'];
 
-        $this->assertEquals(2, $sum);
+        new Library(name: $payload['name'], email: $payload['email']);
+    }
+
+    /** @test */
+    public function should_be_throw_an_exception_if_name_received_is_greater_than_255_characters()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'The name must not be greater than 255 characters'
+        );
+
+        $payload = ['name' => random_bytes(256), 'email' => 'email@library.com'];
+
+        new Library(name: $payload['name'], email: $payload['email']);
+    }
+
+    /** @test */
+    public function should_be_throw_an_excpetion_if_email_received_is_invalid()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The email must be valid');
+
+        $payload = ['name' => 'Library name', 'email' => 'email.com'];
+
+        new Library(name: $payload['name'], email: $payload['email']);
+    }
+
+    /** @test */
+    public function should_be_able_to_create_a_new_library()
+    {
+        $payload = ['name' => 'Library name', 'email' => 'email@library.com'];
+
+        $library = new Library(name: $payload['name'], email: $payload['email']);
+
+        $this->assertNotEmpty($library->id);
+        $this->assertEquals($payload['name'], $library->name);
+        $this->assertEquals($payload['email'], $library->email);
+    }
+
+    /** @test */
+    public function should_be_able_to_change_library_name()
+    {
+        $payload = ['name' => 'Library name updated'];
+
+        $library = new Library(name: 'Library name', email: 'email@library.com');
+        $library->update(name: $payload['name']);
+
+        $this->assertEquals($payload['name'], $library->name);
+    }
+
+    /** @test */
+    public function should_be_able_to_change_library_email()
+    {
+        $payload = ['email' => 'library@email.com'];
+
+        $library = new Library(name: 'Library name', email: 'email@library.com');
+        $library->update(email: $payload['email']);
+
+        $this->assertEquals($payload['email'], $library->email);
+    }
+
+    /** @test */
+    public function should_be_able_to_change_library_datas()
+    {
+        $payload = ['name' => 'Library name updated', 'email' => 'library@email.com'];
+
+        $library = new Library(name: 'Library name', email: 'email@library.com');
+        $library->update(name: $payload['name'], email: $payload['email']);
+
+        $this->assertEquals($payload['name'], $library->name);
+        $this->assertEquals($payload['email'], $library->email);
     }
 }

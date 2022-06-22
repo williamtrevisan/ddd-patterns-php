1f1ef94fe5b0a694964a0e8e7472e0e85b625df2
William Trevisan <williamtrev@hotmail.com>
2022-06-22 14:35:38 -0300

    test: create book factory unit test

---
+++ tests/Unit/Domain/Factory/BookFactoryUnitTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit\Domain\Factory;
+
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+class BookFactoryUnitTest extends TestCase
+{
+    /** @test */
+    public function should_be_able_to_create_a_new_book()
+    {
+        $payload = [
+            'libraryId' => Uuid::uuid4(),
+            'title' => 'Book title',
+            'pageNumber' => 200,
+            'yearLaunched' => 2001
+        ];
+
+        $book = BookFactory::create($payload);
+
+        $this->assertNotEmpty($book->id);
+        $this->assertEquals($payload['libraryId'], $book->libraryId);
+        $this->assertEquals($payload['title'], $book->title);
+        $this->assertEquals($payload['pageNumber'], $book->pageNumber);
+        $this->assertEquals($payload['yearLaunched'], $book->yearLaunched);
+    }
+}
